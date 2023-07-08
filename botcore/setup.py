9a70e80b4cb725ce890504d2de1ef65fed420f5e
h4438 <www.hao2912@gmail.com>
2023-07-08 11:35:32 +0700

    added: setup file for botcore tested: setup file for botcore

---
+++ botcore/setup.py
@@ -0,0 +1,28 @@
+from langchain.llms import VertexAI
+import os
+from dotenv import load_dotenv
+
+def load_my_env():
+    env_path = os.path.dirname(__file__)
+    load_dotenv(f'{env_path}/../.google/env')
+
+# Trace
+
+def enable_tracing(session:str='test-deploy') -> bool:
+    load_my_env()
+    lang_key = os.getenv("LANGCHAIN")
+    os.environ["LANGCHAIN_TRACING_V2"] = "true"
+    os.environ["LANGCHAIN_ENDPOINT"] = "https://api.langchain.plus"
+    os.environ["LANGCHAIN_API_KEY"] = lang_key
+    os.environ["LANGCHAIN_SESSION"] = session
+    print(f"Enable tracing at {session}")
+    return True
+
+def trace_palm2(model_name:str = 'text-bison', max_tokens:int = 128, session:str="test-deploy") -> VertexAI:
+    enable_tracing()
+    service_json_path = f'{os.path.dirname(__file__)}/../.google/service_account.json'
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = service_json_path
+    model = VertexAI(model_name=model_name, max_output_tokens=max_tokens,verbose=True, temperature=0)
+    print("Vertex AI Palm 2 ready")
+    return model
+
---
+++ tests/setup.py
@@ -0,0 +1,8 @@
+import sys
+import os
+sys.path.append(f"{os.path.dirname(__file__)}/../")
+from botcore.setup import trace_palm2
+
+model = trace_palm2()
+
+print(model)
