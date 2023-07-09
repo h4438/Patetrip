ccaa1e66fb5f7bbb1b68be9573d5078aea717b71
h4438 <www.hao2912@gmail.com>
2023-07-09 12:08:35 +0700

    updated: allergen diagnose chain's input

--- botcore/chains/allergen_diagnose_chain.py
+++ botcore/chains/allergen_diagnose_chain.py
@@ -1,13 +1,13 @@
 ALLERGEN_DIAGNOSE_CONST = \
-{"inputs":["problem", "chat_history"],
+{"inputs":["verb","chat_history"],
  "outputs": {"chain": "return 'diagnose'.",
              "allergens": "a js array of allergens.",
              "reason": "a explanation for your diagnose.", 
              "guide": "a helpful guide to avoid the problem and also point out mistake that your patient is making."},
- "template": """You are a food allergist and you have asked your patient some questions about the problem "{problem}".
+ "template": """You are a food allergist and you have asked your patient some questions about the problem.
  {chat_history}
 
-Mission: Based on what your patient have told, please diagose the allergen (or allergens) that causing the problem and explain the reason.
+Mission: Based on what your patient have told, please {verb} the allergen (or allergens) that causing the problem and explain the reason.
 
 {format_instructions}
 
@@ -21,7 +21,7 @@ sys.path.append(f"{os.path.dirname(__file__)}/../..")
 from botcore.utils import build_prompt
 from botcore.setup import trace_palm2
 
-def build_allergen_diagnose_chain(model: BaseLLM, memory):
+def build_allergen_diagnose_chain(model, memory):
     inputs = ALLERGEN_DIAGNOSE_CONST['inputs']
     outputs = ALLERGEN_DIAGNOSE_CONST['outputs']
     template = ALLERGEN_DIAGNOSE_CONST['template']
